4d428e17782c40d6dc4a9b7432087cba1c35d943
anuraggehlot <106402301+anuraggehlot@users.noreply.github.com>
2023-03-24 13:36:55 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYW51dXJhZzA4IiwiYSI6ImNsZmdxZnBheDFjZ3A0MnRhd2MzeWRhaDUifQ.wzRDnqnTt19hL2TuWKiusw"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYW51dXJhZzA4IiwiYSI6ImNsZmdxZnBheDFjZ3A0MnRhd2MzeWRhaDUifQ.wzRDnqnTt19hL2TuWKiusw';
+
+   
+  // Add the control to the map.
+  map.addControl(
+  new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl
+  })
+  );
+}
